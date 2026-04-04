2e2f663951b9c6eb4c1a5364137edfcb181a5a20
gilmer castellanos <gedcastellanosb@gmail.com>
2026-04-04 18:07:55 -0500

    feat toggle del sidebar

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+const menuBtn = document.querySelector('.topbar__menu-btn');
+const sidebar = document.querySelector('.sidebar');
+const wrapper = document.querySelector('.wrapper');
+
+menuBtn.addEventListener('click', function() {
+    sidebar.classList.toggle('sidebar--hidden');
+    wrapper.classList.toggle('wrapper--expanded');
+
+});
