249dd541618854f8a01cf5c2149e37051455da5c
gilmer castellanos <gedcastellanosb@gmail.com>
2026-04-05 20:22:32 -0500

    feat: filtro de ordenes por estado

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,5 +8,40 @@ menuBtn.addEventListener('click', function() {
 
 });
 
+const filterBtns = document.querySelectorAll('.filter-btn');
+const rows = document.querySelectorAll('.orders__table tbody tr');
 
+filterBtns.forEach(function(btn){
+    btn.addEventListener('click', function() {
+
+
+        //quitar clase activa //
+
+        filterBtns.forEach(function(b) {
+            b.classList.remove('filter-btn--active');
+
+        });
+
+        //agregar clase activa haciendo click//
+
+        btn.classList.add('filter-btn--active');
+
+        //leer el filtro elegido
+
+        const filter = btn.dataset.filter;
+
+        //mostrar u ocultar filas
+
+        rows.forEach(function(row) {
+            const estado = row.querySelector('.badge').textContent.trim();
+            if(filter === 'all' || estado === filter) {
+                row.style.display = '';
+            } else {
+                row.style.display = 'none';
+            }
+        });
+
+        
+    });
+});
 
